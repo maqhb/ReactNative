4ae6571692ad4e988b983bf6aa7b65470fc95a51
maqhb <31518888+maqhb@users.noreply.github.com>
2020-04-24 02:08:04 +0500

    Hello World

---
+++ App.js
@@ -0,0 +1,44 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+  Button,
+} from 'react-native';
+
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+const App: () => Test = () => {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center"
+      }}>
+      <Text>Hello, world! I'm Testing this baby</Text>
+      <Button title="Hi" color="#841584" onPress={()=>{alert("Hi Fuck Off"),console.log("Hey")}} />
+    </View>
+  )
+};
+
+
+export default App;
